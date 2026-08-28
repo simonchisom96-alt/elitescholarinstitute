311a8e6274bd1627cddf79a0ac4d3dc92e825cb6
simonchisom96-alt <simonchisom96@gmail.com>
2026-08-28 12:43:32 +0100

    Update downloader.js
    Commit 3

--- downloader.js
+++ downloader.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function(){
-  const CACHE_NAME = 'elite-scholar-v36.2272';
+  const CACHE_NAME = 'elite-scholar-v36.2275';
   const cache = await caches.open(CACHE_NAME);
   const btns = document.querySelectorAll('.download-btn');
 
@@ -119,3 +119,4 @@ document.addEventListener('DOMContentLoaded', async function(){
     });
   });
 });
+      
