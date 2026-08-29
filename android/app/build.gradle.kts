2d529f1d58cedc348b6bbaea77683793e9107a23
simonchisom96-alt <simonchisom96@gmail.com>
2026-08-29 13:05:35 +0100

    Bump Android app to v2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362327
-        versionName = "2.1"
+        versionCode = 362328
+        versionName = "2.2"
     }
 
     buildTypes {
