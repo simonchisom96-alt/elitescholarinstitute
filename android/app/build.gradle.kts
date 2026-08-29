f7cc617026606a6cd96cb4cf1a881fd8967ab927
simonchisom96-alt <simonchisom96@gmail.com>
2026-08-29 17:01:28 +0100

    Bump Android app to v3.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362336
-        versionName = "3.0"
+        versionCode = 362337
+        versionName = "3.1"
     }
 
     buildTypes {
