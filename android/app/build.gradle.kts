e8e39ba458845e03c07d42ba74ef2eea6505fcba
simonchisom96-alt <simonchisom96@gmail.com>
2026-08-29 01:03:45 +0100

    Bump Android APK to clean build 36.2307

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362296
-        versionName = "36.2296"
+        versionCode = 362307
+        versionName = "36.2307"
     }
 
     buildTypes {
