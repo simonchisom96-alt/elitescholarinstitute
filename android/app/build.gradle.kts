d6374b8b553b3b617794f4d87bced37a5a20fa1b
simonchisom96-alt <simonchisom96@gmail.com>
2026-08-30 06:33:59 +0100

    Bump Android APK to v4.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362403
-        versionName = "4.3"
+        versionCode = 362404
+        versionName = "4.4"
     }
 
     buildTypes {
