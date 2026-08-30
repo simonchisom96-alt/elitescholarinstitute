93b1ddb2de178daffabb4460edf58fc8c72df661
simonchisom96-alt <simonchisom96@gmail.com>
2026-08-30 05:12:58 +0100

    Bump Android APK to v3.9 / versionCode 362347

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362346
-        versionName = "3.8"
+        versionCode = 362347
+        versionName = "3.9"
     }
 
     buildTypes {
