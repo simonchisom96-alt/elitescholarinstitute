f5990c582c0a200a83cbe2ffe42ceeb64af05df1
simonchisom96-alt <simonchisom96@gmail.com>
2026-09-06 23:48:27 +0100

    Bump Android app version to 4.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362406
-        versionName = "4.6"
+        versionCode = 362407
+        versionName = "4.7"
     }
 
     buildTypes {
