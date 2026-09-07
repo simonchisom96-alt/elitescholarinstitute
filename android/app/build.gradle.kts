4de2bd6ddca3acc9ddebc06103279b80c898d4b7
simonchisom96-alt <simonchisom96@gmail.com>
2026-09-07 06:55:10 +0100

    Bump Android app to ESI v5.1 for full communication and creator upgrade

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362410
-        versionName = "5.0"
+        versionCode = 362411
+        versionName = "5.1"
     }
 
     buildTypes {
