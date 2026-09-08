df24a0a7764db0b4477631d1624f09e275b3a9a1
simonchisom96-alt <simonchisom96@gmail.com>
2026-09-08 05:53:46 +0100

    chore(android): bump ESI APK to v5.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elitescholarinstitute.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 362417
-        versionName = "5.7"
+        versionCode = 362418
+        versionName = "5.8"
     }
 
     buildTypes {
